63214531892513201e0f2735fa727caad170dc40
Mariusm.koder <mariusm.koder@gmail.com>
2024-02-16 10:08:17 +0100

    piano sounds added

--- js/soundboard_oppgave.js
+++ js/soundboard_oppgave.js
@@ -14,6 +14,9 @@ import {soundFiles} from "../data/soundJSONexample.json"
 //*1. Catch the html element with id drumkit: */
 
 const drumkit = document.getElementById("drumkit");
+const whiteKeys = document.querySelectorAll("whiteKey");
+const blackKeys = document.querySelectorAll("blackKey");
+
 
 
 //* ////////////////////////////////////// */
@@ -68,5 +71,53 @@ const drumkit = document.getElementById("drumkit");
 
 //*4. Call on the function that loops over the sounds and creates the buttons */
 
+// create a drum component:
+const drumComponent = (sound) => {
+    // const soundRef = `${data.sounds.folder}/${sound.file}`
+    const buttonEl = document.createElement('button');
+    buttonEl.textContent = `${sound.file.slice(0, -4)}\n(${sound.key})`;
 
-//* ////////////////////////////////////// */
+    //! koden under gir heller hvilken key som skal trykkes på
+    // buttonEl.textContent = sound.key
+
+    const audioEl = document.createElement('audio');
+    // create an id for each audio element (not needed to make this work, but just for the example)
+    audioEl.id = `hotkey-${sound.key}`
+    audioEl.src = `${data.sounds.folder}/${sound.file}`
+    // audioEl.src = soundRef
+
+    buttonEl.addEventListener('focus', () => {
+        audioEl.play()
+    })
+
+    // buttonEl.addEventListener('pointerup', () => {
+    //     audioEl.pause()
+    //     audioEl.currentTime = 0
+    // })
+
+    document.addEventListener('keydown', (event) => {
+        const key = event.key.toLowerCase();
+        if (key === sound.key.toLowerCase()) {
+            audioEl.play()
+        }
+
+    })
+
+
+    document.addEventListener('keyup', () => {
+        audioEl.pause()
+        audioEl.currentTime = 0
+    })
+    drumkitContainer.append(buttonEl, audioEl)
+}
+
+const createDrumKit = () => {
+    data.sounds.soundsFiles.forEach((sound) => {
+        drumComponent(sound)
+    })
+}
+
+createDrumKit();
+
+
+
